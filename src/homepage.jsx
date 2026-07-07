d4d108a27fe3e017ae13f63a0675c147e62b3743
jamiemlawrence <jameslawrence557@gmail.com>
2026-07-06 18:56:34 -0700

    updated titles of reels

--- src/homepage.jsx
+++ src/homepage.jsx
@@ -1254,7 +1254,7 @@ const VideoPage = () => {
               <div style={{ maxWidth: '100%' }}>
                 <div style={{ padding: '177.78% 0 0 0', position: 'relative', background: 'black' }}>
                   <iframe 
-                    src="https://player.vimeo.com/video/1207582505?badge=0&autopause=0&player_id=0&app_id=58479&title=0&byline=0&portrait=0"
+                    src="https://player.vimeo.com/video/1207582505?badge=0&autopause=0&player_id=0&app_id=58479"
                     frameBorder="0"
                     allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
                     style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
@@ -1267,7 +1267,7 @@ const VideoPage = () => {
               <div style={{ maxWidth: '100%' }}>
                 <div style={{ padding: '177.78% 0 0 0', position: 'relative', background: 'black' }}>
                   <iframe 
-                    src="https://player.vimeo.com/video/1207582657?badge=0&autopause=0&player_id=0&app_id=58479&title=0&byline=0&portrait=0"
+                    src="https://player.vimeo.com/video/1207582657?badge=0&autopause=0&player_id=0&app_id=58479"
                     frameBorder="0"
                     allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
                     style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
